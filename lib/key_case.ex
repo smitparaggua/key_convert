e0e4cacd8a8535f4d6af5af95f27e1fab8ef531b
John Smith Paraggua <smitparaggua@gmail.com>
2018-05-31 20:29:27 +0800

    Added camelize

--- lib/key_case.ex
+++ lib/key_case.ex
@@ -21,4 +21,22 @@ defmodule KeyCase do
   end
 
   defp snake_case_key(key), do: key
+
+  def camelize(map) when is_map(map), do: camelize_key(map)
+
+  defp camelize_key(map) when is_map(map) do
+    for {key, value} <- map, into: %{} do
+      {camelize_key(key), camelize_key(value)}
+    end
+  end
+
+  defp camelize_key(atom) when is_atom(atom) do
+    atom |> to_string() |> camelize_key()
+  end
+
+  defp camelize_key(string) when is_binary(string) do
+    String.first(string) <> (string |> Macro.camelize() |> String.slice(1..-1))
+  end
+
+  defp camelize_key(key), do: key
 end
--- test/key_case_test.exs
+++ test/key_case_test.exs
@@ -15,4 +15,26 @@ defmodule KeyCaseTest do
       }
     end
   end
+
+  describe ".camelize" do
+    test "converts the Map keys to camel case" do
+      assert KeyCase.camelize(%{total_amount: 500}) == %{"totalAmount" => 500}
+    end
+
+    test "converts keys of nested map" do
+      input = %{
+        contact_details: %{
+          phone_number: "555-55-55",
+          email_address: "email@example.com"
+        }
+      }
+
+      assert KeyCase.camelize(input) == %{
+        "contactDetails" => %{
+          "phoneNumber" => "555-55-55",
+          "emailAddress" => "email@example.com"
+        }
+      }
+    end
+  end
 end
