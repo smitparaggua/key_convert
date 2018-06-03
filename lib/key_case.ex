6b61cdd989ad14a6b5707b8ba3df93eeb1d9eeeb
John Smith Paraggua <smitparaggua@gmail.com>
2018-06-03 18:09:15 +0800

    Added doctests

--- lib/key_case.ex
+++ lib/key_case.ex
@@ -1,4 +1,26 @@
 defmodule KeyCase do
+  @moduledoc """
+  KeyCase allows transforming the keys of maps to another case.
+
+  `Atom` keys will be converted to `String`s as atoms are not
+  garbage-collected and are not meant for dynamically generated data.
+
+  It supports nested maps.
+  """
+
+  @doc """
+  Converts the keys to snake case.
+
+  ## Examples
+
+      iex> KeyCase.snake_case(%{totalAmount: 500})
+      %{"total_amount" => 500}
+
+      iex> KeyCase.snake_case(%{
+      ...>   contactInfo: %{emailAddress: "email@example.com"}
+      ...> })
+      %{"contact_info" => %{"email_address" => "email@example.com"}}
+  """
   def snake_case(map) when is_map(map) do
     convert(map, &do_snake_case/1)
   end
@@ -16,6 +38,19 @@ defmodule KeyCase do
 
   defp do_snake_case(key), do: key
 
+  @doc """
+  Converts the keys to camel case.
+
+  ## Examples
+
+      iex> KeyCase.camelize(%{total_amount: 500})
+      %{"totalAmount" => 500}
+
+      iex> KeyCase.camelize(%{
+      ...>   contact_info: %{email_address: "email@example.com"}
+      ...> })
+      %{"contactInfo" => %{"emailAddress" => "email@example.com"}}
+  """
   def camelize(map) when is_map(map) do
     convert(map, &do_camelize/1)
   end
@@ -31,6 +66,15 @@ defmodule KeyCase do
 
   defp do_camelize(key), do: key
 
+  @doc """
+  Converts the keys based `converter` function provided.
+
+  ## Examples
+
+      iex> append_change = fn key -> key <> ".changed" end
+      iex> KeyCase.convert(%{"total_amount" => 500}, append_change)
+      %{"total_amount.changed" => 500}
+  """
   def convert(map, converter) when is_map(map) and is_function(converter, 1) do
     for {key, value} <- map, into: %{} do
       new_key = converter.(key)
