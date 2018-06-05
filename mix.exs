c29c50f5244d136b4820971663f9b84f624138e7
John Smith Paraggua <smitparaggua@gmail.com>
2018-06-05 19:23:46 +0800

    Added project metadata

--- mix.exs
+++ mix.exs
@@ -1,25 +1,41 @@
 defmodule KeyConvert.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/smitparaggua/key_convert"
+
   def project do
     [
       app: :key_convert,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      name: "KeyCase",
+      source_url: @github_url,
+      homepage_url: @github_url,
+      docs: [extras: ["README.md"], main: "readme"],
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["John Smith Paraggua"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @github_url}
+    ]
   end
 end
